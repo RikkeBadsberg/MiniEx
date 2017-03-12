ce84349f5c6457088d22ae6cb255495e9e89495a
RikkeBadsberg <RikkeBadsberg@users.noreply.github.com>
2017-03-12 15:16:31 +0100

    Update sketch.js

--- MiniEx5/sketch.js
+++ MiniEx5/sketch.js
@@ -1,48 +1,118 @@
-var words;
-var input;
+var sentences;
+var ritaWords;
 var lexicon;
 var myFont;
+var selectedLine = 0;
 
-function preload()
-{
-    //load strings from textfile
-    words = loadStrings("data/lyrics.txt");
-    //myFont = loadFont("data/Quicksand-Regular.otf");  //load font
+function preload() {
+    sentences = loadStrings("data/lyrics.txt");
+    myFont = loadFont("data/Quicksand-Regular.otf");  //load font
 }
 
 function setup() {
     createCanvas(900, 600);
     //connect to the RiTa lexicon
     lexicon = new RiLexicon();
-
-    //createText();
+    ritaWords = [];
+    for (var i = 0; i < sentences.length; i++) {
+        ritaWords[i] = procesRita(sentences[i]);
+    }
 }
 
 function draw() {
-    //textFont(myFont);
+    textFont(myFont);
     background(50);
     showText();
+    showChar(sentences[0]);
 }
 
-/*function createText()
-{
-    words = [];
-    words[0] = "BLa bla0";
-    words[1] = "BLa bla1";
-    words[2] = "BLa bla2";
-    words[3] = "BLa bla3";
-    words[4] = "BLa bla4";
-    console.log(words);
-}*/
+function procesRita(textString) {
+    var output = "";
+    var rs = new RiString(textString);
 
-function showText()
-{
-    for (var i = 0; i < words.length; i++) {
+    rs.analyze();
+    var words = rs.words();
+    var pos = rs.pos();
 
-        textSize(30);
-        fill(0, 200, 255);
+    var method = random(0, 5);
+    console.log(method);
 
-        //show text
-        text(words[i], 100, 200 + i * 50);
+    //choose randomly between RiTa features to use
+    if (method < 1) {
+        //Phonemes
+        var phonemes = rs.get(RiTa.PHONEMES);
+        output = phonemes;
+    }
+    else if (method < 2) {
+        //pos
+        output = pos;
+    }
+    else if (method < 3) {
+        //rhymes
+        output = '';
+        for (var i = 0; i < words.length; i++) {
+            var rhyme = lexicon.rhymes(words[i]); //finds and store a word that rhymes with the input word
+            output += rhyme[0]; //adds the rhyming word to the output line
+            output += ' '; //adds a space after the word
+        }
+    }
+    else {
+        //a random word
+        output = '';
+        for (var i = 0; i < words.length; i++) {
+            output += lexicon.randomWord(pos[i]);
+            output += ' '; //adds a space after the word
+        }
+    }
+    return output;
+}
+
+function showText() {
+    for (var i = 0; i < sentences.length; i++) {
+
+        textSize(24);
+        textAlign(LEFT, TOP)
+
+        var line = "";
+        if (i == selectedLine) {
+            fill(0, 255, 000);
+            line = sentences[i]
+        }
+        else {
+            fill(0, random(150, 200), 255);
+            line = ritaWords[i];
+        }
+        text(line, 150, 150 + i * 30);
+    }
+}
+
+function showChar(textLine) {
+    var charWidth = 0;
+    for (var i = 0; i < textLine.length; i++) {
+        fill(0, 200, random(255));
+        textSize(40);
+
+        text(textLine[i], charWidth + 100, 50);
+        charWidth += textWidth(textLine[i]) + 10;
+    }
+}
+
+function keyPressed() {
+    if (keyCode == UP_ARROW) {
+        if (selectedLine == 0) {
+            selectedLine = sentences.length - 1;
+        }
+        else {
+            selectedLine -= 1;
+        }
+    }
+
+    if (keyCode == DOWN_ARROW) {
+        if (selectedLine == (sentences.length - 1)) {
+            selectedLine = 0;
+        }
+        else {
+            selectedLine += 1;
+        }
     }
 }
